ef9cf686d7f59c3b99e7fa41da7dc54bca68e075
Vasil Delchev <vasildelchev97@gmail.com>
2025-05-12 22:22:59 +0300

    Moved data to Infrastructure

--- HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
+++ HouseRentingSystem.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HouseRentingSystem.Web.Data
+namespace HouseRentingSystem.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
