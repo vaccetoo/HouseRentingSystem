9a7985d89b5d099d8eadffef465fbcf0b617d679
Vasil Delchev <vasildelchev97@gmail.com>
2025-05-15 22:13:40 +0300

    Added UnitOfWork

---
+++ HouseRentingSystem.Infrastructure/Data/Common/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Infrastructure.Data.Common
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		IQueryable<TEntity> All<TEntity>() where TEntity : class;
+		IQueryable<TEntity> AllAsNoTracking<TEntity>() where TEntity : class;
+	}
+}
---
+++ HouseRentingSystem.Infrastructure/Data/Common/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data.Common
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly DbContext _context;
+
+		public UnitOfWork(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IQueryable<TEntity> All<TEntity>() where TEntity : class
+			=> _context.Set<TEntity>();
+
+		public IQueryable<TEntity> AllAsNoTracking<TEntity>() where TEntity : class
+			=> _context.Set<TEntity>().AsNoTracking();
+
+		public void Dispose()
+			=> _context.Dispose();
+	}
+}
