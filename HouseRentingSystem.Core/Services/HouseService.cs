c401c983a09c8136fe5658611022c6a8e9e9b10b
Vasil Delchev <vasildelchev97@gmail.com>
2025-05-15 22:14:04 +0300

    Added HouseService

---
+++ HouseRentingSystem.Core/Services/HouseService.cs
@@ -0,0 +1,33 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.Home;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Core.Services
+{
+	public class HouseService : IHouseService
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public HouseService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public async Task<IEnumerable<HouseIndexServiceModel>> LastThreeHouses()
+		{
+			return await _unitOfWork.AllAsNoTracking<House>()
+				.OrderByDescending(h => h.Id)
+				.Take(3)
+				.Select(h => new HouseIndexServiceModel()
+				{
+					Id = h.Id,
+					Title = h.Title,
+					ImageURL = h.ImageURL
+				})
+				.ToListAsync();
+				
+		}
+	}
+}
