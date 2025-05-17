bf92a58cc6bd876ce9d194f197e4d6e7c655cc88
Vasil Delchev <vasildelchev97@gmail.com>
2025-05-17 09:22:30 +0300

    created GetDbSet private method

--- HouseRentingSystem.Infrastructure/Data/Common/UnitOfWork.cs
+++ HouseRentingSystem.Infrastructure/Data/Common/UnitOfWork.cs
@@ -11,13 +11,20 @@ namespace HouseRentingSystem.Infrastructure.Data.Common
 			_context = context;
 		}
 
+		// Returns specific DBSet with change tracker
 		public IQueryable<TEntity> All<TEntity>() where TEntity : class
-			=> _context.Set<TEntity>();
+			=> GetDbSet<TEntity>();
 
+		// eturns specific DBSet without change tracker
 		public IQueryable<TEntity> AllAsNoTracking<TEntity>() where TEntity : class
-			=> _context.Set<TEntity>().AsNoTracking();
+			=> GetDbSet<TEntity>().AsNoTracking();
 
+		// Dispose context when needed
 		public void Dispose()
 			=> _context.Dispose();
+
+		// Returns specific DbSet from context
+		private DbSet<TEntity> GetDbSet<TEntity>() where TEntity : class
+			=> _context.Set<TEntity>();
 	}
 }
