c695539faf8bbdab074b04733af83ae61a9f1d8a
Vasil Delchev <vasildelchev97@gmail.com>
2025-05-17 10:54:12 +0300

    Extended claims principal

---
+++ HouseRentingSystem.Web/Extensions/ClaimsPrincipalExtensons.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Security.Claims
+{
+	public static class ClaimsPrincipalExtensons
+	{
+		public static string? Id(this ClaimsPrincipal user)
+		{
+			return user.FindFirstValue(ClaimTypes.NameIdentifier);
+		}
+		
+	}
+}
