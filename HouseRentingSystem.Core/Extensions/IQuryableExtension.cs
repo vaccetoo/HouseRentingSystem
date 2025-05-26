ea68feb052af8339a18a092a1e0febb7128c5ed6
Vasil Delchev <vasildelchev97@gmail.com>
2025-05-26 07:46:12 +0300

    IQuaryble extension implemented

---
+++ HouseRentingSystem.Core/Extensions/IQuryableExtension.cs
@@ -0,0 +1,21 @@
+﻿using HouseRentingSystem.Core.Models.House;
+using HouseRentingSystem.Infrastructure.Data.Models;
+
+namespace System.Linq
+{
+	public static class IQuryableExtension
+	{
+		public static IQueryable<HouseServiceModel> ProjectToHouseServiceModel(this IQueryable<House> houses)
+		{
+			return houses.Select(h => new HouseServiceModel()
+			{
+				Id = h.Id,
+				Address = h.Address,
+				ImageURL = h.ImageURL,
+				IsRented = h.RenterId != null,
+				PricePerMonth = h.PricePerMonth,
+				Title = h.Title
+			});
+		}
+	}
+}
